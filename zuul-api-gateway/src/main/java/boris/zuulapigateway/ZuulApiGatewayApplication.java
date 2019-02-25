d7b164cdcbb94247b6a3d09eee21c9b1117e1e3a
Borislav Damyanov <borislav.damyanov@devoteam.com>
2019-02-25 11:48:33 +0100

    Implementing of the filters on Zuul is working

--- zuul-api-gateway/src/main/java/boris/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/boris/zuulapigateway/ZuulApiGatewayApplication.java
@@ -1,11 +1,16 @@
 package boris.zuulapigateway;
 
+import boris.zuulapigateway.util.UserContextInterceptor;
 import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -19,6 +24,21 @@ public class ZuulApiGatewayApplication {
     @Bean
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+
+        if(interceptors == null) {
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        } else {
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
     }
 
 }
