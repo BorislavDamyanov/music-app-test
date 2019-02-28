814320279c699e68edf0068bf133d69b7f8b03e1
Borislav Damyanov <borislav.damyanov@devoteam.com>
2019-02-28 10:35:40 +0100

    The security is working (without the common service).

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into users (id, username, firstname, lastname, mail) values ( 1, 'jhonny' , 'Jhon', 'Monroe', 'boris.damyanov@gmail.com');
-insert into users (id, username, firstname, lastname, mail) values ( 2, 'pierre' , 'Pierre', 'Dupont', 'borvil@abv.bg');
-insert into users (id, username, firstname, lastname, mail) values ( 3, 'sophie' , 'Sophie', 'Adams', 'borvil@abv.bg');
+insert into users ( username, firstname, lastname, mail) values (  'jhonny' , 'Jhon', 'Monroe', 'boris.damyanov@gmail.com');
+insert into users ( username, firstname, lastname, mail) values (  'pierre' , 'Pierre', 'Dupont', 'borvil@abv.bg');
+insert into users ( username, firstname, lastname, mail) values (  'sophie' , 'Sophie', 'Adams', 'borvil@abv.bg');
 
 
 
